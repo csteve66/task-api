ae6daa0eb54e950d1b47e1d6cacc4d5c6423454c
Chase Stevens <196700712+csteve66@users.noreply.github.com>
2025-10-28 23:14:57 -0400

    update2

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -2,15 +2,10 @@ import express from 'express';
 import * as taskController from '../controllers/taskController.js';
 import { validateTask } from '../middleware/validateTask.js';
 
-const express = require('express');
 const router = express.Router();
-const taskController = require('../controllers/taskController');
 
 router.get('/', taskController.getTasks);
 
-router.post('/', taskController.createTask);
-
-router.get('/:id', taskController.getTaskById);
-
+router.post('/', validateTask, taskController.createTask);
 
 export default router;
